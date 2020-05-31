85ece6c9ef27c822b24ea72519bd88db6e2aa8d1
Eduardo ARG <eduardo1590@gmail.com>
2020-05-31 10:37:18 -0400

    guardamos firmas en firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, {useRef} from 'react';
 import CanvasDraw from 'react-canvas-draw';
+import firebase from './firebase';
 
 function App() {
+  const [signatures, setSignatures] = React.useState([]);
 
+  React.useEffect(() =>{
+    const fetchData = async () => {
+      const db = firebase.firestore();
+      const data = await db.collection('signatures').get()
+      setSignatures(data.docs.map(doc => doc.data()))
+    }
+    fetchData()
+  }, [])
   const signature = useRef(null);
   const handleClick = () =>{
     const data = signature.current.getSaveData();
     //data debe ser guardada en BD
-    
+    addSign(data);
+  }
+
+  const addSign = (data)=>{
+    const date = new Date();
+    const db = firebase.firestore();
+    db.collection('signatures').add({signature: data, date: date, user: 'Eduardo1590@gmail.com'});
+    clear();
   }
 
   const clear = ()=>{
@@ -16,9 +33,9 @@ function App() {
 
   return (
     <div>
-      <button onClick={handleClick}>Save</button>
-      <button onClick={clear}>Clear</button>
       <CanvasDraw brushRadius={.3} ref={signature}/>
+      <button onClick={handleClick}>Save</button>
+      <button onClick={clear}>Delete</button>
     </div>
   );
 }
