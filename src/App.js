3067a20f0931d8e7de3d8c706e6f01defcb1bbdb
Eduardo ARG <eduardo1590@gmail.com>
2020-05-31 16:45:03 -0400

    mejoras simples

--- src/App.js
+++ src/App.js
@@ -35,9 +35,10 @@ function App() {
   return (
     <div> 
       <CanvasDraw brushRadius={.3} ref={signature} canvasHeight={550}/> 
+      <br/>
       <Row className="justify-content-xs-center">
-        <Col xs={3} md={1}><Button variant="danger" onClick={clear}>Delete</Button></Col>
-        <Col xs={3} md={1}><Button variant="success" onClick={handleClick}>Save</Button></Col>
+        <Col xs={3} md={1}><Button variant="danger" size="lg" onClick={clear}>Delete</Button></Col>
+        <Col xs={3} md={1}><Button variant="success" size="lg" onClick={handleClick}>Save</Button></Col>
       </Row>
      
     </div>
